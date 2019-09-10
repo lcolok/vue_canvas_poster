2e1401a84faa4bfc58ca9d7ba4de329553317487
lcolok(JUB) <425311101@qq.com>
2019-09-10 21:18:57 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const port = 9018; // dev port
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/vue_canvas_poster/" : "/",
+  outputDir:'./docs',
   devServer: {
     port: port,
     open: false,
